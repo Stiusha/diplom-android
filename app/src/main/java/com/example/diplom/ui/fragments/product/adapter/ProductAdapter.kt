b024dd68025b6a8679709a361fb3c6396e3480e2
Leonid Kuzmychenko <lleonidk@ukr.net>
2024-04-09 17:18:35 +0300

    add new product style

--- app/src/main/java/com/example/diplom/ui/fragments/product/adapter/ProductAdapter.kt
+++ app/src/main/java/com/example/diplom/ui/fragments/product/adapter/ProductAdapter.kt
@@ -3,19 +3,23 @@ package com.example.diplom.ui.fragments.product.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.diplom.databinding.ProductItemBinding
-import com.example.diplom.retrofit.dto.Item
+import com.example.diplom.retrofit.dto.ProductDto
 import com.example.diplom.ui.parent.AbstractAdapter
 import com.squareup.picasso.Picasso
 
-class ProductAdapter(dataList: List<Item>, private val clickListener: ProductItemClickListener) :
-    AbstractAdapter<ProductItemBinding, Item>(dataList) {
+class ProductAdapter(
+    dataList: List<ProductDto>,
+    private val clickListener: ProductItemClickListener
+) :
+    AbstractAdapter<ProductItemBinding, ProductDto>(dataList) {
 
-    override fun bindViewHolder(binding: ProductItemBinding, data: Item) {
-        Picasso.get().load(data.getImage()).into(binding.productItemImage)
-        binding.productItemTitle.text = data.getName()
-        binding.categoryItemContainer.setOnClickListener {
-            clickListener.chooseProduct(data.getId())
-        }
+    override fun bindViewHolder(binding: ProductItemBinding, data: ProductDto) {
+        Picasso.get().load(data.productImage).into(binding.productItemImage)
+        binding.productItemTitle.text = data.productName
+        binding.productItemPrice.text = data.productPrice.toString() + "₴"
+//        binding.categoryItemContainer.setOnClickListener {
+//            clickListener.chooseProduct(data.getId())
+//        }
     }
 
     override fun createBinding(inflater: LayoutInflater, parent: ViewGroup): ProductItemBinding {
